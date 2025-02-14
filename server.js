3b89a3055dc028f3c01b20c81910d769a9c7e6d8
Fabio Pretel <fabio_pretel@outlook.com>
2025-02-14 00:38:51 -0300

    save

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
