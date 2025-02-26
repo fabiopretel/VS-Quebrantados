4655fd46dd5037fca5dd30d47d5af0a0a266b67c
Fabio Pretel <fabio_pretel@outlook.com>
2025-02-26 02:41:29 -0300

    save all

--- interacao/menu.js
+++ interacao/menu.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     let botao = document.querySelector(".botaoMenu");
     let menu = document.getElementById("menuLateral");
+    let menuItems = menu.querySelectorAll("a"); // Seleciona todos os links dentro do menu
 
+    // Ao clicar no botão do menu, abre ou fecha o menu
     botao.addEventListener("click", function (event) {
         event.stopPropagation(); // Impede que o clique no botão feche o menu imediatamente
         menu.classList.toggle("menu-aberto");
@@ -12,6 +14,13 @@ document.addEventListener("DOMContentLoaded", function () {
         if (!menu.contains(event.target) && !botao.contains(event.target)) {
             menu.classList.remove("menu-aberto");
         }
+    });
+
+    // Adiciona um evento de clique para cada item do menu
+    menuItems.forEach(function (item) {
+        item.addEventListener("click", function () {
+            menu.classList.remove("menu-aberto"); // Fecha o menu
+        });
     });
 });
 
